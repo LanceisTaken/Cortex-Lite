57dfe1f9227af2003f674b31f1264f9d8ddd750d
Goh Yin Xu <gamermusic63@gmail.com>
2026-07-01 22:33:33 +0800

    [Sprint 1] remove leftover /api/user route from install:api scaffold

--- routes/api.php
+++ routes/api.php
@@ -7,12 +7,7 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::post('/register', [RegisterController::class, 'store'])
     ->middleware('guest')
